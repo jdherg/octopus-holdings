628cf0db138039f176ab63b96610645547aa0868
John Hergenroeder <jdherg@gmail.com>
2018-04-21 18:39:55 -0400

    Split custom emoji mappings from auto-generated mappings.
    This avoids accidentally overwriting custom mappings when emoji mappings are
    regenerated.

--- octopusholdings.py
+++ octopusholdings.py
@@ -4,12 +4,16 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-with open('emoji_map.json', 'r') as f:
-    emoji_dict = json.load(f)
-    emoji_aliases = dict()
-    for emoji in emoji_dict.keys():
-        for alias in emoji_dict[emoji]:
-            emoji_aliases[alias] = emoji
+def load_emoji(filename, emoji_aliases):
+    with open(filename, 'r') as f:
+        emoji_dict = json.load(f)
+        for emoji in emoji_dict.keys():
+            for alias in emoji_dict[emoji]:
+                emoji_aliases[alias] = emoji
+
+emoji_aliases = dict()
+load_emoji('emoji_map.json', emoji_aliases)
+load_emoji('custom_emoji_map.json', emoji_aliases)
 
 
 @app.route('/')
