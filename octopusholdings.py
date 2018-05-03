b398123a645bdfc8bd6d3c31de193470dd892cb9
John Hergenroeder <jdherg@gmail.com>
2018-05-02 21:41:39 -0400

    Support emoji set prioritization.

--- octopusholdings.py
+++ octopusholdings.py
@@ -4,28 +4,43 @@ from flask import abort, Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-def load_emoji_set_metadata(filename, emoji_set_paths):
+def load_emoji_set_metadata(filename):
+    emoji_set_priority = list()
+    emoji_set_config = dict()
+    emoji_set_paths = dict()
     with open(filename, 'r') as f:
         emoji_set_metadata = json.load(f)
-    for metadata in emoji_set_metadata.values():
-        emoji_set_paths[metadata["external_path"]] = metadata["internal_path"]
+    emoji_set_priority.extend(emoji_set_metadata["priority"])
+    for emoji_set_name, config in emoji_set_metadata["config"].items():
+        emoji_set_config[emoji_set_name] = config
+        emoji_set_paths[config["external_path"]] = config["internal_path"]
+    return emoji_set_priority, emoji_set_config, emoji_set_paths
 
-def load_emoji_aliases(filename, emoji_aliases, folder):
-    with open(filename, 'r') as f:
+def load_emoji_aliases(set_config):
+    with open(set_config["alias_map"], 'r') as f:
         emoji_dict = json.load(f)
+    set_aliases = dict()
     for emoji in emoji_dict.keys():
-        emoji_path = "emoji_" + emoji + ".svg"
-        if len(folder):
-            emoji_path = folder + "/" + emoji_path
+        emoji_path = set_config["prefix"] + emoji[1:] + ".svg"
+        if set_config["external_path"]:
+            emoji_path = set_config["external_path"] + "/" + emoji_path
         for alias in emoji_dict[emoji]:
-            emoji_aliases[alias] = emoji_path
+            set_aliases[alias] = emoji_path
+    return set_aliases
 
-emoji_set_paths = dict()
-load_emoji_set_metadata('emoji_set_metadata.json', emoji_set_paths)
+emoji_set_priority, emoji_set_config, emoji_set_paths = load_emoji_set_metadata(
+    'emoji_set_metadata.json')
 
 emoji_aliases = dict()
-load_emoji_aliases('emoji_map.json', emoji_aliases, "noto_1")
-load_emoji_aliases('custom_emoji_map.json', emoji_aliases, "")
+for emoji_set_name in reversed(emoji_set_priority):
+    emoji_aliases[emoji_set_name] = load_emoji_aliases(
+        emoji_set_config[emoji_set_name])
+
+def resolve(alias):
+    for set_name in emoji_set_priority:
+        if alias in emoji_aliases[set_name]:
+            return emoji_aliases[set_name].get(alias)
+    return resolve("question")
 
 @app.route('/')
 @app.route('/<path:emojicode>')
@@ -36,7 +51,7 @@ def show_emoji(emojicode='tada'):
     for i, emoji in enumerate(emojistack):
         if emoji == 'random':
             emoji = random.choice(list(emoji_aliases.keys()))
-        emojistack[i] = emoji_aliases.get(emoji, 'noto/emoji_u2753.svg')
+        emojistack[i] = resolve(emoji)
     return render_template('index.html', stack=emojistack)
 
 
