ff8e6c8b7ea9b3b7258843f0f0672edcdf1dd115
John Hergenroeder <jdherg@gmail.com>
2018-05-13 23:24:29 -0400

    Change title based on OCTOPUS_HOLDINGS_ENV env variable.

--- octopusholdings.py
+++ octopusholdings.py
@@ -1,7 +1,9 @@
+import os
 from flask import abort, Flask, render_template, send_from_directory
 import emoji_resolver
 
 app = Flask(__name__)
+OCTOPUS_HOLDINGS_ENV=os.environ.get("OCTOPUS_HOLDINGS_ENV")
 
 @app.route('/')
 @app.route('/<path:emojicode>')
@@ -25,7 +27,8 @@ def show_emoji(emojicode='tada'):
     # Turn it into paths.
     emojistack = [emoji_resolver.resolve(alias, desired_set)
                   for alias, desired_set in processed_stack]
-    return render_template('index.html', stack=emojistack)
+    return render_template('index.html', stack=emojistack,
+                           env=OCTOPUS_HOLDINGS_ENV)
 
 @app.route('/emoji/<path:filepath>')
 def emoji_src(filepath):
@@ -35,5 +38,7 @@ def emoji_src(filepath):
     abort(404)
 
 if __name__ == '__main__':
+    if not OCTOPUS_HOLDINGS_ENV:
+        OCTOPUS_HOLDINGS_ENV="LOCAL"
     app.debug = True
     app.run()
